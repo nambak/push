458eb765ad20718d40049ba0b8c139d18c3dae5e
alex <alex@10minutesquad.com>
2022-07-26 15:15:06 +0900

    푸시 테스트 유저 동의 여부 조건 추가

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,6 +67,10 @@ class User extends Authenticatable
     {
         return self::whereHas('pushTestUser')
             ->whereNotNull('device_key')
+            ->where([
+                ['send_marketing_push',  1],
+                ['send_push_message', 1]
+            ])
             ->get()
             ->pluck('device_key')
             ->toArray();
